a0859e4ec9929026d1b1227020879fd9cc53fdbc
Juliya Ipatova <inu07040@mail.ru>
2020-12-18 02:31:48 +0300

    Создание DAO, исправление ошибки запуска программы

---
+++ test2/DAO/DAOUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace University.DAO
+{
+    public class DAOUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "192.168.205.130";
+            int port = 3306;
+            string database = "university";
+            string username = "root";
+            string password = "inu07040";
+
+            return DAOString.GetDBConnection(host, port, database, username, password);
+        }
+    }
+}
